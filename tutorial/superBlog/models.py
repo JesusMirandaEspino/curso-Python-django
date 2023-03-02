b438ff5b10e069631bf3db2953ba6fe303169989
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-02 09:00:20 -0600

    blog part 2

--- tutorial/superBlog/models.py
+++ tutorial/superBlog/models.py
@@ -4,5 +4,3 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add=True) # nuevo
-    modified = models.DateTimeField(auto_now=True) # nuevo
