749ce0f07146678cf29bc5d04de2df7b1c811383
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-03 09:26:24 -0600

    post part 2

--- tutorial/superBlog/admin.py
+++ tutorial/superBlog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'modified')
+    list_display = ('title', 'created', 'modified')
+    date_hierarchy = 'created'  # nuevo
+
+admin.site.register(Post, PostAdmin)
