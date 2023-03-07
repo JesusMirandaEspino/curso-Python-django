82d4f8525a88933063939abfff69d5f7e470b681
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-07 09:30:02 -0600

    show posts

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
+
 
 
 def home(request):
-    return render(request, "blog/home.html")
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {'posts': posts})
 # Create your views here.
