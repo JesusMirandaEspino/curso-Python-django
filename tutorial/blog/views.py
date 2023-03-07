622a7dd20c50af46e74f84c77838a1f93de5050d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-07 09:22:02 -0600

    html url

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
+
+def home(request):
+    return HttpResponse("<h1>Bienvenidos a mi blog</h1>")
 # Create your views here.
