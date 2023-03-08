bf4f414ace9a61bfb58694e4475c2a0815f0a5f0
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-08 08:52:56 -0600

    post get one blog

--- tutorial/blog/views.py
+++ tutorial/blog/views.py
@@ -7,3 +7,8 @@ def home(request):
     posts = Post.objects.all()
     return render(request, "blog/home.html", {'posts': posts})
 # Create your views here.
+
+
+def post(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "blog/post.html", {'post': post})
