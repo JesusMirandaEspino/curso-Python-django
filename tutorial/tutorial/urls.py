5f8b6555c3a3c6c2dc9bd8de878320b2b123650e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-09 09:39:33 -0600

    styles part 2

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog.views import home, post
+from django.conf import settings             # nuevo
+from django.conf.urls.static import static   # nuevo
 
 urlpatterns = [
     path('', home, name="home"),          # editado
     path('blog/<id>', post, name="post"),  # editado
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # nuevo
